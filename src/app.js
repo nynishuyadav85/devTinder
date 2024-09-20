299c0d7aab39fe2137dcefd5749b8b5f3bc8661b
Nishant Yadav <nynishuyadav85@gmail.com>
2024-09-20 12:58:23 +0530

    Initial changes for the project 1. Created a server using express 2. Installed nodemon for automatic start of a server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/test',(req, res) => {
+
+    res.send('Hello from the server dekho idahr bhai aa gya mai salee  !!!!!')
+})
+
+app.listen(3000, () => {
+    console.log("App is running succesuflly on port 3000.....")
+})
