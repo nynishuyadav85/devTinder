59a5f4a9dd013968c59fd3b877ae49d62c6dfc2b
Nishant Yadav <nynishuyadav85@gmail.com>
2024-09-24 12:15:09 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -4,26 +4,48 @@ const userSchema = new mongooes.Schema({
   firstName: {
     type: String,
     required: true,
+    minLength: 4,
+    maxLength: 50,
   },
   lastName: {
     type: String,
   },
   age: {
     type: Number,
+    min: 18,
   },
   gender: {
     type: String,
+    validate(value) {
+      if (!["male", "female", "other"].includes(value)) {
+        throw new Error("Gender data is not valid");
+      }
+    },
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    lowercase: true,
+    trim: true,
   },
   password: {
     type: String,
+    required: true,
   },
   city: {
     type: String,
+  },
+  photoUrl: {
+    type: String,
+    default: "Upload Photo",
+  },
+  about: {
+    type: String,
+    default: "This is default about you ",
+  },
+  skills: {
+    type: [String],
   },
 });
 
